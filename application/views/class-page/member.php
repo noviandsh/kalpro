9f723e78bdd438276d5766580de2dafdfb154522
novian dwi syahrizal hilmi <noviandsh@gmail.com>
2020-05-25 19:09:30 +0700

    new member page

--- application/views/class-page/member.php
+++ application/views/class-page/member.php
@@ -1,13 +1,33 @@
-Dosen: <br>
-<span><?=$class[0]['teacher']?></span><br><br>
+<h2>Dosen: </h2>
+<div style="height: 100px;">
+    <div class="member-list">
+        <?php
+            foreach($user as $a){
+                if($class[0]['teacher'] == $a['name']){
+                    echo "<img src='".$a['photo']."'>";
+                }
+            }
+            ?>
+        <div><?=$class[0]['teacher']?></div><br><br>
+    </div>
+</div>
 
-Member:
+<h2>Member:</h2>
 <ul> 
-    <?php
-    foreach($member as $mhs){
-        if($mhs['classID'] == $class[0]['classID']){
-            echo "<li>".$mhs['username']."</li>";
+    
+        <?php
+        foreach($member as $mhs){
+            if($mhs['classID'] == $class[0]['classID']){
+                echo "<li>
+                    <div class='member-list'>";
+                    foreach($user as $a){
+                        if($mhs['username'] == $a['name']){
+                            echo "<img src='".$a['photo']."'>";
+                        }
+                    }
+                echo "<div>".$mhs['username']."</div></div></li>";
+            }
         }
-    }
-    ?>
+        ?>
+    </div>
 </ul>
