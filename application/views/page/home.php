dbb3dbea62aee593c7cd863c9f9731477e64fbc4
novian dwi syahrizal hilmi <noviandsh@gmail.com>
2020-05-25 23:46:12 +0700

    add jquery modal & remove message and archive

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -6,16 +6,19 @@
     <title>Beranda | Kalpro</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="<?=base_url('assets/css/main-style.css')?>">
-    <link href="<?php echo base_url('assets/css/all.min.css');?>" rel="stylesheet"/>
-    <link href="<?php echo base_url('assets/css/jquery-ui.css');?>" rel="stylesheet"/>
-    <link href="<?php echo base_url('assets/css/jquery-ui.theme.css');?>" rel="stylesheet"/>
-    <link href="<?php echo base_url('assets/css/jquery-ui-timepicker-addon.css');?>" rel="stylesheet"/>
+    <link href="<?=base_url('assets/css/all.min.css');?>" rel="stylesheet"/>
+    <link href="<?=base_url('assets/css/jquery-ui.css');?>" rel="stylesheet"/>
+    <link href="<?=base_url('assets/css/jquery-ui.theme.css');?>" rel="stylesheet"/>
+    <link href="<?=base_url('assets/css/jquery-ui-timepicker-addon.css');?>" rel="stylesheet"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" rel="stylesheet"/>
+    <link href="<?=base_url('assets/img/circ-favicon-v2.png');?>" rel="shortcut icon" type="text/css">
 
     <script src="<?=base_url('assets/js/jquery.min.js')?>"></script>
     <script src="<?=base_url('assets/js/jquery-ui.js')?>"></script>
     <script src="<?=base_url('assets/js/jquery-ui-timepicker-addon.js')?>"></script>
     <script src="<?=base_url('assets/js/jquery-collision.js')?>"></script>
     <script src="<?=base_url('assets/js/jquery.connections.js')?>"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
 
 </head>
 <body>
@@ -29,8 +32,8 @@
             </div>
             <div id="menubar-bawah">
                     <div class="menu"><a href="<?=base_url('dataprocess/logout')?>"><p><i class="fas fa-sign-out-alt fa-sm"></i> Logout</p></a></div>
-                    <div class="menu"><a href="<?=base_url('message');?>"><p><i class="fas fa-comments fa-sm"></i> Pesan</p></a></div>
-                    <div class="menu"><a href="<?=base_url('archive');?>"><p><i class="fas fa-file-archive fa-sm"></i> Arsip</p></a></div>
+                    <!-- <div class="menu"><a href="<?=base_url('message');?>"><p><i class="fas fa-comments fa-sm"></i> Pesan</p></a></div>
+                    <div class="menu"><a href="<?=base_url('archive');?>"><p><i class="fas fa-file-archive fa-sm"></i> Arsip</p></a></div> -->
                     <div class="menu"><a href="<?=base_url('class');?>"><p><i class="fas fa-school fa-sm"></i> Kelas</p></a></div>
                     <div class="active menu"><a href="<?=base_url();?>"><p><i class="fas fa-home fa-sm"></i> Beranda</p></a></div>
                 </div>
@@ -38,11 +41,11 @@
                 <div id="menu-dropdown-par">
                     <div id="logo2"></div>
                     <div id="menu-dropdown">
-                    <div class="active menu"><a href="<?php echo base_url();?>"><p>Home</p></a></div>
-                    <div class="menu"><a href="<?php echo base_url('article');?>"><p>Article</p></a></div>
-                    <div class="menu"><a href="<?php echo base_url('tutorial');?>"><p>Tutorial</p></a></div>
-                    <div class="menu"><a href="<?php echo base_url('tipsntrick');?>"><p>Tips & Trick</p></a></div>
-                    <div class="menu"><a href="<?php echo base_url('about');?>"><p>About</p></a></div>
+                    <div class="active menu"><a href="<?=base_url();?>"><p>Home</p></a></div>
+                    <div class="menu"><a href="<?=base_url('article');?>"><p>Article</p></a></div>
+                    <div class="menu"><a href="<?=base_url('tutorial');?>"><p>Tutorial</p></a></div>
+                    <div class="menu"><a href="<?=base_url('tipsntrick');?>"><p>Tips & Trick</p></a></div>
+                    <div class="menu"><a href="<?=base_url('about');?>"><p>About</p></a></div>
                 </div>
             </div>
         </div>
