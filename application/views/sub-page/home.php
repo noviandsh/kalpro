f3aeea72386bf0185011c4822f199dfdabde12c1
novian dwi syahrizal hilmi <noviandsh@gmail.com>
2020-05-25 23:47:27 +0700

    new home style

--- application/views/sub-page/home.php
+++ application/views/sub-page/home.php
@@ -1,14 +1,15 @@
 <div id="container">
     <div id="profile">
         <div class="photo">
-            <img src="<?=base_url('assets/img/images.png')?>" alt="">
-        </div>
-        login as <b><?=$this->session->username;?></b><br>
+            <img src="<?=$acc['photo'];?>" alt="">
+        </div><br>
+        <b><?=$acc['name'];?></b><br>
         <?php if($this->session->type == 'm'){ ?>
-        <form action="<?=base_url('dataprocess/joinClass')?>" method="post">
-            <input type="text" name="classID" placeholder="Masukkan Kode Kelas">
-            <button class="styled-btn">Gabung Kelas</button>
-        </form>
+            <br><hr><br>
+            <form action="<?=base_url('dataprocess/joinClass')?>" method="post">
+                <input type="text" name="classID" placeholder="Masukkan Kode Kelas"><br><br>
+                <button class="styled-btn" data-icon='&#xf549'>Gabung Kelas</button>
+            </form>
         <?php } 
         if(!empty($this->session->joinStat)){
             echo $this->session->joinStat;
@@ -23,9 +24,13 @@
                 <select name="classID">
                     <?php
                         $className = array();
-                        foreach($class as $val){
-                            echo "<option value=".$val['classID'].">".$val['name']."</option>";
-                            $className[$val['classID']] = $val['name'];
+                        if(!empty($class)){
+                            foreach($class as $val){
+                                echo "<option value=".$val['classID'].">".$val['name']."</option>";
+                                $className[$val['classID']] = $val['name'];
+                            }
+                        }else{
+                            echo "<option disabled selected>- Kosong -</option>";
                         }
                     ?>
                 </select>
@@ -33,28 +38,51 @@
             </form>
         </div>
             <?php
-                foreach($feed as $val){
-                    ?>
-                    <div class="feed-container">
-                        <b><?=$val['sender']?></b> mengirim ke <b><?=$className[$val['classID']]?></b><br>
-                        <small><?=$val['date']?></small><br>
-                        <?=$val['content']?>
-                        <div class="comment-container">
-                            <div class="comment-box-<?=$val['id']?>">
-                                <div class="user-comment-1">
-
+            
+                if(!empty($class)){
+                    if(!empty($feed)){
+                        foreach($feed as $val){
+                            ?>
+                            <div class="feed-container">
+                                <div class="feed-photo">
+                                    <div>
+                                        <?php
+                                            foreach($allAcc as $a){
+                                                if($val['sender'] == $a['name']){
+                                                    echo "<img src='".$a['photo']."'>";
+                                                }
+                                            }
+                                        ?>
+                                    </div>
+                                    <div>
+                                        <b><?=$val['sender']?></b> mengirim ke <b><?=$className[$val['classID']]?></b><br>
+                                        <small><?=$val['date']?></small><br>
+                                    </div>
                                 </div>
+                                <div class="feed-content">
+                                    <?=$val['content']?>
+                                </div>
+                                <div class="comment-container">
+                                    <div class="comment-box-<?=$val['id']?>">
+                                        <div class="user-comment-1">
+
+                                        </div>
+                                    </div>
+                                    <div class="comment-form">
+                                        <form action="<?=base_url('dataprocess/comment')?>" method="post">
+                                            <input type="hidden" name="feedID" value="<?=$val['id']?>">
+                                            <input type="hidden" name="prevLink" value="home">
+                                            <textarea name="comment" placeholder="Tulis Komentar..."></textarea><button class="styled-btn" data-icon="&#xf1d8">Kirim</button>
+                                        </form>
+                                    </div>
+                                </div>
+                                <?php if($val['sender'] == $this->session->name || $this->session->type == 'd'): ?>
+                                    <div class="dots" data-delete="kiriman" data-id="<?=$val['id']?>"><i class="fas fa-trash-alt fa-xs"></i></div>
+                                <?php endif; ?>
                             </div>
-                            <div class="comment-form">
-                                <form action="<?=base_url('dataprocess/comment')?>" method="post">
-                                    <input type="hidden" name="feedID" value="<?=$val['id']?>">
-                                    <input type="hidden" name="prevLink" value="home">
-                                    <textarea name="comment" placeholder="Tulis Komentar..."></textarea><button class="styled-btn" data-icon="&#xf1d8">Kirim</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
+                            <?php
+                        }
+                    }
                 }
             ?>
     </div>
@@ -76,6 +104,12 @@
         ?>
     </div>
 </div>
+
+<div id="modal" class="modal">
+    <p>Apakah anda ingin menghapus <span></span> ini?</p><br><br>
+    <button delete-type="" delete-id="" id="delete-btn" class="styled-btn" data-icon='&#xf2ed' style="margin-right: 10px;">Hapus</button>
+    <a style="color: #6b6a6a;" href="#" rel="modal:close">Batal</a>
+</div>
 <script>
     let feed = [<?php echo '"'.implode('","', $feedID).'"' ?>];
     feed.forEach(showComment);
@@ -87,29 +121,37 @@
             contentType: 'application/json',
             dataType: 'json',
             success : function(data){
-                console.log(data.length);
                 let html = '';
                 let i;
                 let arrId = [];
+                let dots;
+                let photo;
                 for(i=0; i<data.length; i++){
+                    if(data[i].sender == '<?=$this->session->name?>' || '<?=$this->session->type?>' == 'd'){
+                        dots = '<div class="dots" data-delete="komentar" data-id="'+data[i].id+'"><i class="fas fa-trash-alt fa-xs"></i></div>';
+                    }else{
+                        dots = '';
+                    }
+                    <?php foreach ($allAcc as $val): ?>
+                        if(data[i].sender == '<?=$val['name']?>'){
+                            photo = '<?=$val['photo']?>';
+                        }
+                    <?php endforeach; ?>
                     html += '<div class="user-comment user-comment-'+data[i].id+'">'+
-                                '<b>'+data[i].sender+'</b><br>'+
-                                '<small>'+data[i].date+'</small><br>'+
+                                '<div class="feed-photo">'+
+                                    '<div>'+
+                                        '<img src="'+photo+'" alt="">'+
+                                    '</div>'+
+                                    '<div>'+
+                                        '<b>'+data[i].sender+'</b><br>'+
+                                        '<small>'+data[i].date+'</small><br>'+
+                                    '</div>'+
+                                '</div>'+
                                 data[i].comment+
-                                '<div><i class="fas fa-ellipsis-v fa-xs"></i></div>'+
+                                dots+
                             '</div><hr>';
-                            // '<td><input id="tipe-'+data[i].id+'" name="tipe-'+data[i].id+'" value="'+data[i].tipe+'"></td>'+
-                            // '<td><input id="harga-'+data[i].id+'" name="harga-'+data[i].id+'" value="'+data[i].harga+'"></td>'+
-                            // '<td><button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myModal2" data-nama="'+data[i].tipe+'" data-id="'+data[i].id+'" data-delete="one">'+
-                            //     '<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>'+
-                            // '</button></td>'+
-                            // '</tr>';
-                            // '<tr ><td><input name="id-'+data[i].id+'" value="'+data[i].id+'"></td></tr>';
-                    // arrId.push(data[i].id);
                 }
-                // html += '<tr><td>Data Baru<input name="id" hidden value="'+arrId+'"></td></tr>';
                 $('.comment-box-'+item).html(html);
-                // $('#hapus-semua').attr('data-id', id);
             }
 
         });
@@ -118,4 +160,32 @@
     function test() {
         console.log($('#blas').val());
     }
+    $('.dots').click(function() {
+        $("#modal").modal({
+            fadeDuration: 100
+        });
+        $("#modal span").html($(this).attr('data-delete'));
+        $("#delete-btn").attr('delete-type', $(this).attr('data-delete'));
+        $('#delete-btn').attr('delete-id', $(this).attr('data-id'));
+    });
+    $('#delete-btn').click(function(){
+        let id = $(this).attr('delete-id');
+        let table = $(this).attr('delete-type');
+        $.ajax({
+            type  : 'POST',
+            url   : '<?=base_url()?>dataprocess/deletepost',
+            // dataType: 'json',
+            data : {table:table, id:id},
+            error: function (jqXHR, textStatus, errorThrown){
+                alert(errorThrown.status);
+            },
+            success : function(data){
+                if(data == 1){
+                    window.location = "<?=base_url()?>";
+                }else{
+                    alert('Gagal menghapus');
+                }
+            }
+        });
+    });
 </script>
