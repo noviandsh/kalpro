44b0459e2b65521709f6a3f30e121e80ab526888
novian dwi syahrizal hilmi <noviandsh@gmail.com>
2020-06-02 22:05:06 +0700

    hide menu & add animation

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -31,25 +31,33 @@
                 </div>
             </div>
             <div id="menubar-bawah">
+                <div id="menu-div">
                     <div class="menu"><a href="<?=base_url('dataprocess/logout')?>"><p><i class="fas fa-sign-out-alt fa-sm"></i> Logout</p></a></div>
-                    <!-- <div class="menu"><a href="<?=base_url('message');?>"><p><i class="fas fa-comments fa-sm"></i> Pesan</p></a></div>
-                    <div class="menu"><a href="<?=base_url('archive');?>"><p><i class="fas fa-file-archive fa-sm"></i> Arsip</p></a></div> -->
-                    <div class="menu"><a href="<?=base_url('class');?>"><p><i class="fas fa-school fa-sm"></i> Kelas</p></a></div>
-                    <div class="active menu"><a href="<?=base_url();?>"><p><i class="fas fa-home fa-sm"></i> Beranda</p></a></div>
+                    <!-- <div class="menu message-menu"><a href="<?=base_url('message');?>"><p><i class="fas fa-comments fa-sm"></i> Pesan</p></a></div>
+                    <div class="menu archive-menu"><a href="<?=base_url('archive');?>"><p><i class="fas fa-file-archive fa-sm"></i> Arsip</p></a></div> -->
+                    <div class="menu class-menu"><a href="<?=base_url('class');?>"><p><i class="fas fa-school fa-sm"></i> Kelas</p></a></div>
+                    <div class="menu home-menu"><a href="<?=base_url();?>"><p><i class="fas fa-home fa-sm"></i> Beranda</p></a></div>
                 </div>
                 <i id="menu-bar" class="fa fa-bars fa-2x" aria-hidden="true"></i>
                 <div id="menu-dropdown-par">
                     <div id="logo2"></div>
-                    <div id="menu-dropdown">
-                    <div class="active menu"><a href="<?=base_url();?>"><p>Home</p></a></div>
-                    <div class="menu"><a href="<?=base_url('article');?>"><p>Article</p></a></div>
-                    <div class="menu"><a href="<?=base_url('tutorial');?>"><p>Tutorial</p></a></div>
-                    <div class="menu"><a href="<?=base_url('tipsntrick');?>"><p>Tips & Trick</p></a></div>
-                    <div class="menu"><a href="<?=base_url('about');?>"><p>About</p></a></div>
+                    <div id="menu-dropdown"> 
+                    <div class="menu home-menu"><a href="<?=base_url();?>"><p>Beranda</p></a></div>
+                    <div class="menu class-menu"><a href="<?=base_url('class');?>"><p>Kelas</p></a></div>
+                    <!-- <div class="menu archive-menu"><a href="<?=base_url('archive');?>"><p>Arsip</p></a></div>
+                    <div class="menu message-menu"><a href="<?=base_url('message');?>"><p>Pesan</p></a></div> -->
+                    <div class="menu"><a href="<?=base_url('dataprocess/logout');?>"><p>Logout</p></a></div>
                 </div>
             </div>
         </div>
     </div>
     <?=$page?>
+    <script>
+        let menu = '<?=$this->uri->segment(1)?>';
+        if(menu==''){
+            menu = 'home';
+        }
+        $('.'+menu+'-menu').addClass('active');
+    </script>
 </body>
 </html>
