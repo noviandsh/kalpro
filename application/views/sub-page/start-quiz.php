ffdda5e1d848344e16170bd9b6b29c8966d4218a
novian dwi syahrizal hilmi <noviandsh@gmail.com>
2020-06-02 20:35:19 +0700

    remove quiz number

--- application/views/sub-page/start-quiz.php
+++ application/views/sub-page/start-quiz.php
@@ -74,17 +74,17 @@
                 <div class="empty"></div>
                 <div class="empty"></div>
             </div>
-            <div id="add-line"><button class="styled-btn" onClick="makeTarget()">+</button></div>
+            <div id="add-line"><button class="styled-btn" data-icon='&#xf067' onClick="makeTarget()">Tambah baris</button></div>
         </div>
     </div>
     <div id="start-quiz-number">
-        <ul>
+        <!-- <ul>
             <li><a class="selected" href="#">1</a></li>
             <li><a href="#">2</a></li>
             <li><a href="#">3</a></li>
             <li><a href="#">4</a></li>
             <li><a href="#">5</a></li>
-        </ul>
+        </ul> -->
     </div>
 </div>
 
