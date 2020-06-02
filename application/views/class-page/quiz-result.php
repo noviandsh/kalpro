f11702dc3d30a6fec6cbb2014f1ee4b6c780bd10
novian dwi syahrizal hilmi <noviandsh@gmail.com>
2020-06-02 22:06:03 +0700

    quiz result

---
+++ application/views/class-page/quiz-result.php
@@ -0,0 +1,28 @@
+<div id="quiz-result-container">
+    <table>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Jawaban Benar</th>
+                <th>Nilai</th>
+                <th>Menu</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $n = 1;
+                foreach($result as $res){
+                    echo "<tr>
+                            <td>".$n."</td>
+                            <td>".$res['username']."</td>
+                            <td>".$res['correctAnswer']."</td>
+                            <td>".$res['score']."</td>
+                            <td>daosdksa</td>
+                        </tr>";
+                    $n++;
+                }
+            ?>
+        </tbody>
+    </table>
+</div>
