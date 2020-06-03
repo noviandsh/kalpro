f02dd63c473b653dfbc15f4125ada100bc2d4260
novian dwi syahrizal hilmi <noviandsh@gmail.com>
2020-06-03 11:27:48 +0700

    hide duration

--- application/views/sub-page/quiz-result.php
+++ application/views/sub-page/quiz-result.php
@@ -2,7 +2,8 @@
     <div id="result-card">
         <div id="quiz-result-title" style="font-size: 30px;"><?=$quizDetail[0]['title']?></div>
         <div id="quiz-result-detail" style="font-size: 12px;">
-            Waktu mulai <?=$userAnswer[0]['startTime']?> | Durasi <?=$quizDetail[0]['duration']?> Menit
+            Waktu mulai <?=$userAnswer[0]['startTime']?> 
+            <!-- | Durasi <?=$quizDetail[0]['duration']?> Menit -->
         </div><br><br>
         Nilai anda <?=$result[0]['score']?>
     </div>
